8678f1fb303bee175c6c11341e111745d04d0a86
jonnymariani <jonnymariani@hotmail.com>
2022-02-16 00:28:33 -0300

    Convert project to .net core 5.0

---
+++ JMClicker/CleanNumericUpDown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JMClicker
+{
+    public class CleanNumericUpDown : NumericUpDown
+    {
+        public CleanNumericUpDown()
+        {
+            Controls[0].Hide();
+        }
+
+        protected override void OnTextBoxResize(object source, EventArgs e)
+        {
+            Controls[1].Width = Width + Controls[0].Width;
+        }
+    }
+}
